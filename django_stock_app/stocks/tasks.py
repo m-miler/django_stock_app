9fc5a3229dbeec5331479298df7889a9572a1f8c
m-miler <mmiler30@gmail.com>
2022-12-30 14:59:20 +0100

    fix: code refactoring

--- django_stock_app/stocks/tasks.py
+++ django_stock_app/stocks/tasks.py
@@ -4,6 +4,24 @@ from stocks.models.companies_model import StockCompanies
 from api.serializers.stock_price_serializer import StockPricesSerializer
 import requests
 
+DB_COLUMNS = ['date', 'open_price', 'max_price', 'min_price', 'close_price', 'volume']
+
+
+def get_stock_data(company, start_day):
+    url = f'https://stooq.pl/q/d/l/?s={company}&d1={start_day}&d2={start_day}&i=d'
+    data = {'company_abbreviation': company}
+
+    response = requests.get(url=url).content.decode('utf-8').strip().split('\r\n')
+    data.update(dict(zip(DB_COLUMNS, response[1].split(','))))
+    return data
+
+
+def save_data(company, start_day):
+    data = get_stock_data(company, start_day)
+    serializer = StockPricesSerializer(data=data)
+    serializer.is_valid()
+    serializer.save()
+
 
 @shared_task
 def price_db_update(*args, **kwargs):
@@ -11,7 +29,6 @@ def price_db_update(*args, **kwargs):
 
     companies = StockCompanies.objects.values('company_abbreviation')
     start_day = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y%m%d')
-    db_columns = ['date', 'open_price', 'max_price', 'min_price', 'close_price', 'volume']
 
     for abbreviation in companies:
         company = abbreviation.get('company_abbreviation')
