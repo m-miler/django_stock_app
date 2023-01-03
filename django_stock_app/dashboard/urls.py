040c8e9e07286e5034ecd616dd17d48ca421fd5c
m-miler <mmiler30@gmail.com>
2023-01-03 22:54:27 +0100

    fix: code refactoring

--- django_stock_app/dashboard/urls.py
+++ django_stock_app/dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth.forms import UsernameField
+from dashboard.views import Dashboard, home
 
 urlpatterns = [
     path('dashboard/<str:ticker>', Dashboard.as_view(), name='dashboard'),
