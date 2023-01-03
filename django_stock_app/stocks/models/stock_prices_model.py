9ff56d3a96994b79ac4a4532abb3003e0dc588da
m-miler <mmiler30@gmail.com>
2023-01-03 22:54:08 +0100

    feat: add a mathematical calculation to the database and to the templates

--- django_stock_app/stocks/models/stock_prices_model.py
+++ django_stock_app/stocks/models/stock_prices_model.py
@@ -1,5 +1,9 @@
+from datetime import datetime, timedelta
 from django.db import models
 from stocks.models.companies_model import StockCompanies
+
+TODAY_DATE = '2022-12-29' #(datetime.date(datetime.today()) - timedelta(days=1)).strftime('%Y-%m-%d')
+LAST_WEEK_END = '2022-12-22'#(datetime.date(datetime.today()) - timedelta(days=7)).strftime('%Y-%m-%d')
 
 
 class StockPrices(models.Model):
@@ -14,3 +18,34 @@ class StockPrices(models.Model):
     close_price = models.FloatField(help_text='Day Close Price')
     volume = models.BigIntegerField(help_text='Day Volume')
 
+    @property
+    def last_week_price(self):
+        last_week_price = StockPrices.objects.filter(models.Q(date__contains=LAST_WEEK_END) &
+                                                    models.Q(company_abbreviation__company_abbreviation__contains=
+                                                             self.company_abbreviation)).first()
+        return last_week_price
+
+    @property
+    def close_weekly_change(self):
+        weekly_change = round(((self.close_price - self.last_week_price.close_price) / self.close_price), 4)
+        return weekly_change * 100
+
+    @property
+    def open_weekly_change(self):
+        weekly_change = round(((self.open_price - self.last_week_price.open_price) / self.open_price), 4)
+        return weekly_change * 100
+
+    @property
+    def max_weekly_change(self):
+        weekly_change = round(((self.max_price - self.last_week_price.max_price) / self.max_price), 4)
+        return weekly_change * 100
+
+    @property
+    def min_weekly_change(self):
+        weekly_change = round(((self.min_price - self.last_week_price.min_price) / self.min_price), 4)
+        return weekly_change * 100
+
+    @property
+    def volume_weekly_change(self):
+        weekly_change = round(((self.volume - self.last_week_price.volume) / self.volume), 4)
+        return weekly_change * 100
