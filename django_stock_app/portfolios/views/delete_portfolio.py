f12e26b4bfd1312d3515c5a66eac1070964040a4
m-miler <mmiler30@gmail.com>
2023-01-07 17:30:29 +0100

    feat: add delete function to the portfolios application

---
+++ django_stock_app/portfolios/views/delete_portfolio.py
@@ -0,0 +1,23 @@
+from django.views.generic import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from portfolios.models.portfolio_model import Portfolio
+
+
+class DeletePortfolio(LoginRequiredMixin, DeleteView):
+    model = Portfolio
+    template_name = 'portfolios/delete_portfolio.html'
+    slug_url_kwarg = 'portfolio'
+    slug_field = 'portfolio_id'
+    success_url = '/'
+
+    def get_object(self, queryset=None, *args, **kwargs):
+        portfolio_id = self.request.user.username + '_' + self.kwargs.get(self.slug_url_kwarg)
+        obj = Portfolio.objects.filter(portfolio_id__contains=portfolio_id).first()
+        return obj
+
+    def get_context_data(self, **kwargs):
+        context = super(DeletePortfolio, self).get_context_data(**kwargs)
+        context['object'] = self.object
+        context['portfolios'] = Portfolio.objects.filter(user__username__contains=self.request.user.username).all()
+        return context
+
