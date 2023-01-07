6372c030a3a3296705d97d9845d1b713038f30dc
m-miler <mmiler30@gmail.com>
2023-01-07 15:53:53 +0100

    feat: add portfolio and index plotly charts

--- django_stock_app/portfolios/charts/portfolio_charts.py
+++ django_stock_app/portfolios/charts/portfolio_charts.py
@@ -7,6 +7,8 @@ def portfolio_chart(data):
     for stock in data:
         df_data = df_data.append({'stock': stock.stock, 'pricing': stock.pricing}, ignore_index=True)
 
+    df_data.sort_values(ascending=False, by=['pricing'], inplace=True)
+
     fig = px.bar(df_data, x='stock', y='pricing',
                  color_discrete_sequence=[px.colors.qualitative.G10[5]])
     fig.update_layout(height=450,
