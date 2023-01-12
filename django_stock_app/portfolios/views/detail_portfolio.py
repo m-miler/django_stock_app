bd6c66a2c3cf2b48437daec51aac36157e523340
m-miler <mmiler30@gmail.com>
2023-01-13 00:21:43 +0100

    feat: add get_totals method for get the aggregation of the models property such as profit/loss, pricing, balance

--- django_stock_app/portfolios/views/detail_portfolio.py
+++ django_stock_app/portfolios/views/detail_portfolio.py
@@ -24,5 +24,23 @@ class PortfolioDetailed(LoginRequiredMixin, DetailView):
         context['portfolios'] = Portfolio.objects.filter(user__username=self.request.user.username).all()
         context['portfolio_detail'] = Portfolio.objects.filter(portfolio_id=portfolio_id).first()
         context['portfolio_stocks'] = portfolio_stocks_queryset
+        context['totals'] = self.get_totals(portfolio_stocks_queryset, portfolio_id)
         context['chart'] = portfolio_chart(portfolio_stocks_queryset)
         return context
+
+    def get_totals(self, queryset, portfolio_id):
+        total_pricing = sum([stock.pricing for stock in queryset])
+        total_portfolio_pricing = sum([stock.portfolio_pricing for stock in queryset])
+        total_profit_loss = sum([stock.profit_loss for stock in queryset])
+        total = total_pricing + Portfolio.objects.filter(portfolio_id=portfolio_id).first().balance
+        if total_portfolio_pricing > 0:
+            total_profit_loss_percentage = round(total_profit_loss / total_portfolio_pricing, 2) * 100
+        else:
+            total_profit_loss_percentage = 0
+        context_dict = {'total_pricing': total_pricing,
+                        'total_portfolio_pricing': total_portfolio_pricing,
+                        'total_profit_loss': total_profit_loss,
+                        'total_profit_loss_percentage': total_profit_loss_percentage,
+                        'total': total
+                        }
+        return context_dict
