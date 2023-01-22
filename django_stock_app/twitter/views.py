19d07d47d45763b2431a9907cc8cb69d95b1865b
m-miler <mmiler30@gmail.com>
2023-01-22 22:57:10 +0100

    feat: add a twitter application

---
+++ django_stock_app/twitter/views.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+import tweepy
+
+TWITTER_CREDENTIALS = {
+    'bearer_token': settings.BEARER_TOKEN,
+    'api_key': settings.API_KEY,
+    'api_key_secret': settings.API_KEY_SECRET,
+    'access_token': settings.ACCESS_TOKEN,
+    'access_token_secret': settings.ACCESS_TOKEN_SECRET
+}
+
+
+def search_tweets(ticker):
+    query = f'#{ticker} #{ticker.lower()} #GPW #gpw lang:pl'
+    tweet_fields = ['author_id', 'created_at', 'text']
+    client = tweepy.Client(
+        bearer_token=TWITTER_CREDENTIALS.get('bearer_token'),
+        consumer_key=TWITTER_CREDENTIALS.get('api_key'),
+        consumer_secret=TWITTER_CREDENTIALS.get('api_key_secret'),
+        access_token=TWITTER_CREDENTIALS.get('access_token'),
+        access_token_secret=TWITTER_CREDENTIALS.get('access_token_secret')
+    )
+
+    tweets = client.search_recent_tweets(query, max_results=10,
+                                         tweet_fields=tweet_fields,
+                                         )
+
+    return tweets
