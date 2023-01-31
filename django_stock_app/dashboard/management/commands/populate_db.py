39fdd4b174c8ef0c77914c11f2192aeb4249d5c4
m-miler <mmiler30@gmail.com>
2023-01-31 16:51:53 +0100

    feat: add a new command to populate database with some sample data

---
+++ django_stock_app/dashboard/management/commands/populate_db.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from stocks import factories as stock_factories
+from portfolios import factories
+
+
+class Command(BaseCommand):
+    """Command to populate database with some sample data."""
+    help = 'Populate database with sample data'
+
+    def handle(self, *args, **kwargs):
+        users = factories.UserFactory.create_batch(2)
+        for user in users:
+            factories.PortfolioFactory.create_batch(2, portfolio_id=user)
+
+        companies = stock_factories.CompanyFactory.create_batch(5)
+        for company in companies:
+            stock_factories.StockPriceFactory.reset_sequence(1, force=True)
+            stock_factories.StockPriceFactory.create_batch(30, company_abbreviation=company)
+
+        wig = stock_factories.CompanyFactory(company_abbreviation='WIG20', company_full_name='WIG20')
+        stock_factories.StockPriceFactory.reset_sequence(1, force=True)
+        stock_factories.StockPriceFactory.create_batch(30, company_abbreviation=wig)
